1bc396311cb4c8a6570fbdc85938cdea646b7019
Joker Hacker <jokerhacker.6521@protonmail.com>
2021-06-09 23:48:18 +0530

    Minor changes in nmap module and it is still in development

--- userbot/modules/nmap.py
+++ userbot/modules/nmap.py
@@ -7,12 +7,20 @@ from userbot.events import register
 async def nmap_fun(event):
     await event.edit("`Processing...`")
     nm = nmap.PortScanner()
-    ip = event.pattern_match.group(1)
-    port = event.pattern_match.group(2)
-    result = nm.scan(ip, port)
+    await event.edit("Scanning now...")
+    for host in nm.all_hosts():
+        result = '----------------------------------------------------'
+        result += 'Host : %s (%s)' % (host, nm[host].hostname())
+        result += 'State : %s' % nm[host].state()
+        for proto in nm[host].all_protocols():
+            result += '----------'
+            result += 'Protocol : %s' % proto
 
-    await event.edit(result)
-
+            lport = nm[host][proto].keys()
+            lport.sort()
+            for port in lport:
+                result += ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+                await event.edit(result)
 
 CMD_HELP.update({
     "nmap":
